5a2098ed3415c9d03afdc878671fc4bcff3727ed
hwee <gee6121@naver.com>
2026-06-09 13:14:50 +0900

    feat: 3장 GET API 단일 데이터 조회

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pip._internal.cli import status_codes
 
 from schema.response import TodoResponse
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 
 app = FastAPI()
+
 
 # 할 일 저장
 todos = [
@@ -13,6 +14,14 @@ todos = [
 ]
 
 # 전체 할 일 조회
-@app.get("/todos", response_model=list[TodoResponse], status_codes=status.HTTP_200_OK)
+@app.get("/todos", response_model=list[TodoResponse], status_code=status.HTTP_200_OK)
 def get_todos_handler():
     return todos
+
+# 단일 할 일 조회
+@app.get("/todos/{todo_id}", response_model=TodoResponse, status_code=status.HTTP_200_OK)
+def get_todo_handler(todo_id: int):
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo Not found")
