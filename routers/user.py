370c89b8b4eb098020f01416d88203e88d81c03e
hwee <gee6121@naver.com>
2026-06-21 13:19:15 +0900

    feat: 5장 회원가입 요청 데이터 검증 및 이메일 중복 검사 구현

--- routers/user.py
+++ routers/user.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
+from sqlalchemy import select
+from schema.request import UserSignRequest
+from database.db_connection import SessionFactory
+from models import User
 
 router = APIRouter(tags=["User"])
 
 # 회원가입
-@router.get("/users/signup", status_code=status.HTTP_201_CREATED)
+@router.post("/users/signup", status_code=status.HTTP_201_CREATED)
 
-def signup_user_handler():
+def signup_user_handler(body: UserSignRequest):
+    # 이메일 중복 검사
+    with SessionFactory() as session:
+        stmt = select(User).where(User.email == body.email)
+        existing_user = session.scalars(stmt)
+        if existing_user :
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 사용 중인 이메일입니다.")
     """
     회원가입 처리 과정
     1. 요청 데이터 검증
