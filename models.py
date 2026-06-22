9cbdeaf6629f7e6b6d0f98fa27d6d05d8f0cef32
hwee <gee6121@naver.com>
2026-06-22 23:45:41 +0900

    fix: 오류 구문 수정

--- models.py
+++ models.py
@@ -17,12 +17,12 @@ class Todo(Base):
 
     user: Mapped[User] = relationship("User", back_populates="todos")
 
-    # User 모델
-    class User(Base):
-        __tablename__ = 'user'
+# User 모델
+class User(Base):
+    __tablename__ = 'user'
 
-        id : Mapped[int] = mapped_column(Integer, primary_key=True)
-        email : Mapped[String] = mapped_column(String(255),unique=True,index=True, nullable=False)
-        hashed_password : Mapped[String] = mapped_column(String(255),nullable=False)
-        created_at : Mapped[DateTime] = mapped_column(DateTime,server_default=func.now(),nullable=False)
-        todos: Mapped[list["Todo"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    email : Mapped[String] = mapped_column(String(255),unique=True,index=True, nullable=False)
+    hashed_password : Mapped[String] = mapped_column(String(255),nullable=False)
+    created_at : Mapped[DateTime] = mapped_column(DateTime,server_default=func.now(),nullable=False)
+    todos: Mapped[list["Todo"]] = relationship(back_populates="user", cascade="all, delete-orphan")
