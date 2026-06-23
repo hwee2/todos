63d313fe83b2f3bd3b92a15c147b697944daca3c
hwee <gee6121@naver.com>
2026-06-23 22:05:21 +0900

    feat: 6장 authorization 헤더 기반 시용자 인증하기 구현

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,12 +1,28 @@
 import jwt
 from datetime import datetime, timedelta,timezone
+from fastapi import HTTPException, status
+
 
 SECRET_KEY = "your-secret-here"
 ALGORITHM = "HS256"
 
+#액세스 토큰 생설
 def create_access_token(user_id: int, expires_minutes: int):
     payload = {
         "user_id": user_id,
         "exp": datetime.now(timezone.utc) + timedelta(minutes=expires_minutes),
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+# 액세스 토큰 검증 및 사용자 정보 추출
+def decode_access_token(token: str) -> int:
+    try:
+        #토큰 디코딩 및 사용자 아이디 추출
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload["user_id"]
+    #토큰 만료 예외처리
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail= "Token expired")
+    # 유효하지 않은 토큰 예외 처리
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
--- routers/todo.py
+++ routers/todo.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, APIRouter, Request
+from fastapi import HTTPException, APIRouter, Request, Depends
 from sqlalchemy import select
 from starlette import status
 
@@ -6,27 +6,37 @@ from database.db_connection import SessionFactory
 from models import Todo
 from schema.request import TodoCreateRequest, TodoUpdateRequest
 from schema.response import TodoResponse
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from auth.jwt import decode_access_token
+
 
 router = APIRouter(tags=["Todo"])
+bearer = HTTPBearer(auto_error=False)
 
 # 전체 할 일 조회
 @router.get("/todos", response_model=list[TodoResponse], status_code=status.HTTP_200_OK)
-def get_todos_handler(request: Request):
-    user_id = request.session.get("user_id")
-    session = SessionFactory() # 요청 단위 세션 생성
+def get_todos_handler(authorization: HTTPAuthorizationCredentials | None = Depends(bearer)):
+    user_id = None
+    if authorization:
+        token = authorization.credentials
+        user_id = decode_access_token(token)
+    session = SessionFactory()
     try:
-        stmt = select(Todo).where(Todo.user_id == user_id) # 전체 조회 쿼리 객체 생성
-        todos = session.execute(stmt).scalars().all() # 쿼리 실행 및 결과 변환
-        return todos # 결과 반환
+        stmt = select(Todo).where(Todo.user_id == user_id)
+        todos = session.execute(stmt).scalars().all()
+        return todos
     finally:
-        session.close() # 세션 종료
-
+        session.close()
 
 # 단일 할 일 조회
 @router.get("/todos/{todo_id}", response_model=TodoResponse, status_code=status.HTTP_200_OK)
-def get_todo_handler(request: Request, todo_id: int):
-    user_id = request.session.get("user_id")
+def get_todo_handler(todo_id: int, authorization: HTTPAuthorizationCredentials | None = Depends(bearer)):
+    user_id = None
+    if authorization:
+        token = authorization.credentials
+        user_id = decode_access_token(token)
     session = SessionFactory()
+
     try:
         stmt = select(Todo).where(Todo.id == todo_id, Todo.user_id == user_id) #단일 조회 쿼리 객체 생성
         todo = session.execute(stmt).scalars().first() # 쿼리 실행 및 단일 결과 선택
@@ -39,8 +49,11 @@ def get_todo_handler(request: Request, todo_id: int):
 
 #할 일 생성
 @router.post("/todos", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
-def create_todo_handler(request: Request, body: TodoCreateRequest): # 요청 본문 매개변수로 받기
-    user_id = request.session.get("user_id") # 세션에서 사용자 아이디 조회
+def create_todo_handler(body: TodoCreateRequest, authorization: HTTPAuthorizationCredentials | None = Depends(bearer)): # 요청 본문 매개변수로 받기
+    user_id = None
+    if authorization:
+        token = authorization.credentials
+        user_id = decode_access_token(token)
     session = SessionFactory()
     try:
         todo = Todo( # ORM 모델 객체 생성
@@ -57,8 +70,11 @@ def create_todo_handler(request: Request, body: TodoCreateRequest): # 요청 본
 
 # 할 일 수정
 @router.patch("/todos/{todo_id}", response_model=TodoResponse, status_code=status.HTTP_200_OK)
-def update_todo_handler(request:Request, todo_id: int, body: TodoUpdateRequest):
-    user_id = request.session.get("user_id")
+def update_todo_handler(todo_id: int, body: TodoUpdateRequest, authorization: HTTPAuthorizationCredentials | None = Depends(bearer)):
+    user_id = None
+    if authorization:
+        token = authorization.credentials
+        user_id = decode_access_token(token)
     session = SessionFactory()
     try:
         stmt = select(Todo).where(Todo.id == todo_id, Todo.user_id == user_id) # 수정 대상 조회 쿼리 객체 생성
@@ -79,8 +95,11 @@ def update_todo_handler(request:Request, todo_id: int, body: TodoUpdateRequest):
 
 # 할 일 삭제
 @router.delete("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo_handler(request:Request, todo_id: int):
-    user_id = request.session.get("user_id")
+def delete_todo_handler(todo_id: int, authorization: HTTPAuthorizationCredentials | None = Depends(bearer)):
+    user_id = None
+    if authorization:
+        token = authorization.credentials
+        user_id = decode_access_token(token)
     session = SessionFactory()
     try:
         stmt = select(Todo).where(Todo.id == todo_id, Todo.user_id == user_id)
